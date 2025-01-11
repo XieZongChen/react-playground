8460bf1933ed607186573e9cd8dc60715b9d0c4c
XieZongChen <952937570@qq.com>
2025-01-11 23:24:03 +0800

    feat: 解决 import 普通文件导入的问题

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,44 @@
-import { useRef } from 'react';
 import { transform } from '@babel/standalone';
+import type { PluginObj } from '@babel/core';
 
 function App() {
-  const textareaRef = useRef<HTMLTextAreaElement>(null);
+  const code1 = `
+    function add(a, b) {
+        return a + b;
+    }
+    export { add };
+    `;
+
+  /**
+   * import 的 url 可以用 blob url
+   * 所以这里通过 Bob 和 URL.createObjectURL 的方式把 import 的文件内容变为一个 blob url
+   */
+  const url = URL.createObjectURL(
+    new Blob([code1], { type: 'application/javascript' })
+  );
+
+  const transformImportSourcePlugin: PluginObj = {
+    visitor: {
+      ImportDeclaration(path) {
+        // 在 Ast 时，将 import 的路径换成生成的 blob url
+        path.node.source.value = url;
+      },
+    },
+  };
+
+  const code = `import { add } from './add.ts'; console.log(add(2, 3));`;
 
   function onClick() {
-    if (!textareaRef.current) {
-      return;
-    }
-
-    const res = transform(textareaRef.current.value, {
+    const res = transform(code, {
       presets: ['react', 'typescript'],
-      filename: 'test.tsx',
+      filename: 'test.ts',
+      plugins: [transformImportSourcePlugin],
     });
     console.log(res.code);
   }
 
-  const code = `import { useEffect, useState } from "react";
-
-  function App() {
-    const [num, setNum] = useState(() => {
-      const num1 = 1 + 2;
-      const num2 = 2 + 3;
-      return num1 + num2
-    });
-  
-    return (
-      <div onClick={() => setNum((prevNum) => prevNum + 1)}>{num}</div>
-    );
-  }
-  
-  export default App;
-  `;
   return (
     <div>
-      <textarea
-        ref={textareaRef}
-        style={{ width: '500px', height: '300px' }}
-        defaultValue={code}
-      ></textarea>
       <button onClick={onClick}>编译</button>
     </div>
   );
