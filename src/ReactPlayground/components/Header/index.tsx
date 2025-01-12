1aedc71623b3b0ecade549287d7d0fd231efc4ee
XieZongChen <952937570@qq.com>
2025-01-12 23:06:02 +0800

    feat: header 样式

--- src/ReactPlayground/components/Header/index.tsx
+++ src/ReactPlayground/components/Header/index.tsx
@@ -1,9 +1,11 @@
 import logoSvg from '@/assets/logo.svg';
 
+import styles from './index.module.scss';
+
 export default function Header() {
   return (
-    <div>
-      <div>
+    <div className={styles.header}>
+      <div className={styles.logo}>
         <img alt='logo' src={logoSvg} />
         <span>React Playground</span>
       </div>
