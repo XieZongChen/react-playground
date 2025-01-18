177e81d50dcedbf5536aa917920d7be3188b8074
XieZongChen <952937570@qq.com>
2025-01-18 21:59:35 +0800

    feat: 引入 PlaygroundProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
-import ReactPlayground from './ReactPlayground';
 import './App.scss';
+import { PlaygroundProvider } from './ReactPlayground/PlaygroundContext';
+import ReactPlayground from './ReactPlayground';
 
 function App() {
-  return <ReactPlayground />;
+  return (
+    <PlaygroundProvider>
+      <ReactPlayground />
+    </PlaygroundProvider>
+  );
 }
 
 export default App;
