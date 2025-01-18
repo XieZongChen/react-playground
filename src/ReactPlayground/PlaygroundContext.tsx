0d61094ca282fd945817c6e3e3786b9673dee709
XieZongChen <952937570@qq.com>
2025-01-18 21:56:50 +0800

    feat: 开发 PlaygroundProvider

--- src/ReactPlayground/PlaygroundContext.tsx
+++ src/ReactPlayground/PlaygroundContext.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, PropsWithChildren, useState } from 'react';
 
 export interface File {
   name: string;
@@ -11,8 +11,8 @@ export interface Files {
 }
 
 export interface PlaygroundContext {
-  files: Files;
-  selectedFileName: string;
+  files: Files; // 文件信息
+  selectedFileName: string; // 当前选择的文件
   setSelectedFileName: (fileName: string) => void;
   setFiles: (files: Files) => void;
   addFile: (fileName: string) => void;
@@ -20,6 +20,64 @@ export interface PlaygroundContext {
   updateFileName: (oldFieldName: string, newFieldName: string) => void;
 }
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const PlaygroundContext = createContext<PlaygroundContext>({
   selectedFileName: 'App.tsx',
 } as PlaygroundContext);
+
+export const PlaygroundProvider = (props: PropsWithChildren) => {
+  const { children } = props;
+  const [files, setFiles] = useState<Files>({});
+  const [selectedFileName, setSelectedFileName] = useState('App.tsx');
+
+  const addFile = (name: string) => {
+    files[name] = {
+      name,
+      language: fileName2Language(name),
+      value: '',
+    };
+    setFiles({ ...files });
+  };
+
+  const removeFile = (name: string) => {
+    delete files[name];
+    setFiles({ ...files });
+  };
+
+  const updateFileName = (oldFieldName: string, newFieldName: string) => {
+    if (
+      !files[oldFieldName] ||
+      newFieldName === undefined ||
+      newFieldName === null
+    )
+      return;
+    const { [oldFieldName]: value, ...rest } = files;
+    const newFile = {
+      [newFieldName]: {
+        ...value,
+        language: fileName2Language(newFieldName),
+        name: newFieldName,
+      },
+    };
+    setFiles({
+      ...rest,
+      ...newFile,
+    });
+  };
+
+  return (
+    <PlaygroundContext.Provider
+      value={{
+        files,
+        selectedFileName,
+        setSelectedFileName,
+        setFiles,
+        addFile,
+        removeFile,
+        updateFileName,
+      }}
+    >
+      {children}
+    </PlaygroundContext.Provider>
+  );
+};
