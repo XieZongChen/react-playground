d73617964cdf7835ba1bd51b4627dc92824aca7d
XieZongChen <952937570@qq.com>
2025-01-18 22:01:14 +0800

    feat: 开发 FileNameList

--- src/ReactPlayground/components/CodeEditor/FileNameList/index.tsx
+++ src/ReactPlayground/components/CodeEditor/FileNameList/index.tsx
@@ -1,3 +1,21 @@
+import { useContext, useEffect, useState } from 'react';
+import { PlaygroundContext } from '../../../PlaygroundContext';
+
 export default function FileNameList() {
-  return <div>FileNameList</div>;
+  const { files, removeFile, addFile, updateFileName, selectedFileName } =
+    useContext(PlaygroundContext);
+
+  const [tabs, setTabs] = useState(['']);
+
+  useEffect(() => {
+    setTabs(Object.keys(files));
+  }, [files]);
+
+  return (
+    <div>
+      {tabs.map((item, index) => (
+        <div>{item}</div>
+      ))}
+    </div>
+  );
 }
