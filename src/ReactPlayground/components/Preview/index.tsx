a81e3e61204fb13bfac10692849c4e657b65801f
XieZongChen <952937570@qq.com>
2025-02-15 12:19:48 +0800

    feat: 错误信息展示逻辑开发

--- src/ReactPlayground/components/Preview/index.tsx
+++ src/ReactPlayground/components/Preview/index.tsx
@@ -1,12 +1,26 @@
 import { useContext, useEffect, useState } from 'react';
 import { PlaygroundContext } from '../../PlaygroundContext';
+// import Editor from '../CodeEditor/Editor';
 import { compile } from './compiler';
 import iframeRaw from './iframe.html?raw';
 import { IMPORT_MAP_FILE_NAME } from '../../files';
+import { Message } from '../Message';
+
+interface MessageData {
+  data: {
+    type: string;
+    message: string;
+  };
+}
 
 export default function Preview() {
   const { files } = useContext(PlaygroundContext);
   const [compiledCode, setCompiledCode] = useState('');
+
+  useEffect(() => {
+    const res = compile(files);
+    setCompiledCode(res);
+  }, [files]);
 
   const getIframeUrl = () => {
     const res = iframeRaw
@@ -21,16 +35,28 @@ export default function Preview() {
     return URL.createObjectURL(new Blob([res], { type: 'text/html' }));
   };
 
-  const [iframeUrl, setIframeUrl] = useState(getIframeUrl());
-
-  useEffect(() => {
-    const res = compile(files);
-    setCompiledCode(res);
-  }, [files]);
-
   useEffect(() => {
     setIframeUrl(getIframeUrl());
   }, [files[IMPORT_MAP_FILE_NAME].value, compiledCode]);
+
+  const [iframeUrl, setIframeUrl] = useState(getIframeUrl());
+
+  const [error, setError] = useState('');
+
+  const handleMessage = (msg: MessageData) => {
+    const { type, message } = msg.data;
+    if (type === 'ERROR') {
+      setError(message);
+    }
+  };
+
+  useEffect(() => {
+    // 监听 iframe 发送的消息
+    window.addEventListener('message', handleMessage);
+    return () => {
+      window.removeEventListener('message', handleMessage);
+    };
+  }, []);
 
   return (
     <div style={{ height: '100%' }}>
@@ -43,6 +69,13 @@ export default function Preview() {
           border: 'none',
         }}
       />
+      <Message type='error' content={error} />
+
+      {/* <Editor file={{
+            name: 'dist.js',
+            value: compiledCode,
+            language: 'javascript'
+        }}/> */}
     </div>
   );
 }
