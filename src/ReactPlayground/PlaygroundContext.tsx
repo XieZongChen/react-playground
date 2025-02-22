1ef3c52ff1406764ff20b0841b29ab89e69f910d
XieZongChen <952937570@qq.com>
2025-02-22 17:13:47 +0800

    feat: provider 增加 theme

--- src/ReactPlayground/PlaygroundContext.tsx
+++ src/ReactPlayground/PlaygroundContext.tsx
@@ -33,6 +33,7 @@ export const PlaygroundProvider = (props: PropsWithChildren) => {
   const { children } = props;
   const [files, setFiles] = useState<Files>(initFiles);
   const [selectedFileName, setSelectedFileName] = useState('App.tsx');
+  const [theme, setTheme] = useState<Theme>('light');
 
   const addFile = (name: string) => {
     files[name] = {
