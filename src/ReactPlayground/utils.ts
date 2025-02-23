3e9101ffdf11d8b4977e6547a5b32850be18caac
XieZongChen <952937570@qq.com>
2025-02-23 14:15:12 +0800

    feat: 使用 fflate 减少 url 记录体积

--- src/ReactPlayground/utils.ts
+++ src/ReactPlayground/utils.ts
@@ -1,3 +1,5 @@
+import { strFromU8, strToU8, unzlibSync, zlibSync } from 'fflate';
+
 /**
  * 通过文件名获取到文件的语言类型
  */
@@ -9,3 +11,20 @@ export const fileName2Language = (name: string) => {
   if (['css'].includes(suffix)) return 'css';
   return 'javascript';
 };
+
+export function compress(data: string): string {
+  const buffer = strToU8(data);
+  const zipped = zlibSync(buffer, { level: 9 });
+  const str = strFromU8(zipped, true);
+  // 将 ASC 码转换为 base64
+  return btoa(str);
+}
+
+export function uncompress(base64: string): string {
+  // 将 base64 转换为 ASC 码
+  const binary = atob(base64);
+
+  const buffer = strToU8(binary, true);
+  const unzipped = unzlibSync(buffer);
+  return strFromU8(unzipped);
+}
