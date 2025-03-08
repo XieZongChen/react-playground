4524e46400c7f858c1c781217701b4b7f10acd21
XieZongChen <952937570@qq.com>
2025-03-08 22:38:30 +0800

    chore: 部署配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'node:path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/react-playground/',
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  }
-})
+  },
+  build: {
+    outDir: 'docs',
+  },
+});
